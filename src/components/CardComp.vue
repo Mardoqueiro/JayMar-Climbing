058db659480e22a6d05ca6d8610a2e02a16bf9f3
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-08-20 14:59:33 +0200

    Comp

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>
-        
+    <div class="card">
+        <div class="card-header">
+            <slot name="cardHeader"></slot>
+        </div>
+        <div class="card-body">
+            <slot name="cardBody"></slot>
+        </div>
     </div>
 </template>
+
 <script>
 export default {
-    
+    name: "CardComp"
 }
 </script>
-<style>
-    
+
+<style scoped>
+.card {
+    width: 18rem;
+}
 </style>
--- src/components/SpinnerComp.vue
+++ src/components/SpinnerComp.vue
@@ -1,13 +1,12 @@
 <template>
-    <div>
-        
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+        </div>
     </div>
 </template>
 <script>
 export default {
-    
+    name: 'SpinnerComp'
 }
 </script>
-<style>
-    
-</style>
+
